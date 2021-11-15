b38127d3403bf636d202eda0318b3dafb693edc1
frinkiac-7.net <rob@frinkiac-7.net>
2021-11-15 14:45:18 -0500

    Add TailwindCSS to project   - Steps required:     * npm install -D tailwindcss     * npm install postcss@latest autoprefixer@latest     * touch tailwind.config.js       + Add base config as included in commit     * Update styles.css to import TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	purge: {
+		enabled: true,
+		content: [
+			'./src/**/*.html',
+			'./src/**/*.css'
+		]
+	},
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: []
+}
