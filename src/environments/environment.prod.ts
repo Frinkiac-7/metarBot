6e12fe5bc00143ce8ddbbde99c55d5fb463e85c7
frinkiac-7.net <rob@frinkiac-7.net>
2021-11-19 15:47:35 -0500

    Add Supabase info to production environment   * URL and API key added to file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+	supabaseUrl:  'https://ojhqmmvvaftbeyjkkrvj.supabase.co',
+	supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzAwODY3NCwiZXhwIjoxOTUyNTg0Njc0fQ.xbom4xH2Eb0z6dt7Fx2zaCkZ3zRsP7-CfluEXVJ4xZ8'
 };
